244732e68e6d9831d33d2c6de602154e81259554
Jannik Glückert <jannik.glueckert@gmail.com>
2024-05-22 15:11:49 +0200

    theme change & fix tag names

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,19 +1,19 @@
-AUTHOR = 'Jannik Glückert'
-SITENAME = 'EBADBLOG'
-SITESUBTITLE = 'A blog about weirdly named POSIX error codes and more'
-SITEURL = ''
-HEADER_COVER = 'images/errno.jpg'
+AUTHOR = "Jannik Glückert"
+SITENAME = "EBADBLOG"
+SITESUBTITLE = "A blog about weirdly named POSIX error codes and more"
+SITEURL = ""
+HEADER_COVER = "images/errno.jpg"
 
-THEME = 'theme/pelican-clean-blog'
+THEME = "theme/pelican-clean-blog"
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'Europe/Berlin'
+TIMEZONE = "Europe/Berlin"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
-COLOR_SCHEME_CSS = 'darkly.css'
-CSS_OVERRIDE = 'css/custom.css'
+COLOR_SCHEME_CSS = "monokai.css"
+CSS_OVERRIDE = "css/custom.css"
 DISABLE_CUSTOM_THEME_JAVASCRIPT = True
 
 FEED_ALL_ATOM = None
@@ -22,22 +22,27 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-SOCIAL = (
-    ('github', 'https://github.com/Jannik2099'),
-)
+SOCIAL = (("github", "https://github.com/Jannik2099"),)
 
 DEFAULT_PAGINATION = 10
 
-STATIC_PATHS = ['css', 'images']
+STATIC_PATHS = ["css", "images"]
 
 MARKDOWN = {
-    'extension_configs': {
-        'markdown.extensions.extra': {},
-        'markdown.extensions.meta': {},
-        'markdown.extensions.codehilite': {
-            'guess_lang': 'false',
-            'css_class': 'highlight',
+    "extension_configs": {
+        "markdown.extensions.extra": {},
+        "markdown.extensions.meta": {},
+        "markdown.extensions.codehilite": {
+            "guess_lang": "false",
+            "css_class": "highlight",
         },
     },
-    'output_format': 'html5',
+    "output_format": "html5",
 }
+
+SLUG_REGEX_SUBSTITUTIONS = [
+    (r"[^\w\s+-]", ""),  # remove non-alphabetical/whitespace/'+-' chars
+    (r"(?u)\A\s*", ""),  # strip leading whitespace
+    (r"(?u)\s*\Z", ""),  # strip trailing whitespace
+    (r"[\s]+", "-"),  # reduce multiple whitespace to single '-'
+]
